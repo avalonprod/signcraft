13888dcd6c783aa81ea733d7ab8bd2ec1c895c74
avalonprod <abramenkoroman22@gmail.com>
2023-11-19 04:34:36 +0300

    add new feature generate uuid

--- claims.go
+++ claims.go
@@ -1,33 +1,9 @@
 package signcraft
 
-import (
-	"encoding/json"
-)
-
 type Claims map[string]interface{}
 
 func New() *Claims {
 	return &Claims{}
-}
-
-func ToClaims(payload interface{}) (Claims, error) {
-	structBytes, _ := json.Marshal(payload)
-	var claims Claims
-	err := json.Unmarshal(structBytes, &claims)
-	if err != nil {
-		return nil, err
-	}
-
-	return claims, nil
-}
-
-func (c Claims) ToStruct(payload interface{}) error {
-	claimsBytes, _ := json.Marshal(c)
-	err := json.Unmarshal(claimsBytes, payload)
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 // func (c Claims) Validate() error {
--- pub.go
+++ pub.go
@@ -2,8 +2,10 @@ package signcraft
 
 import (
 	"crypto/hmac"
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"strings"
@@ -64,4 +66,48 @@ func Verify(token string, secret []byte) bool {
 	mac.Write([]byte(fmt.Sprintf("%s.%s", tokenArray[0], tokenArray[1])))
 	sig, _ := base64.RawURLEncoding.DecodeString(tokenArray[2])
 	return hmac.Equal(sig, mac.Sum(nil))
+}
+
+func ToClaims(payload interface{}) (Claims, error) {
+	structBytes, _ := json.Marshal(payload)
+	var claims Claims
+	err := json.Unmarshal(structBytes, &claims)
+	if err != nil {
+		return nil, err
+	}
+
+	return claims, nil
+}
+
+func (c Claims) ToStruct(payload interface{}) error {
+	claimsBytes, _ := json.Marshal(c)
+	err := json.Unmarshal(claimsBytes, payload)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GenerateUUID() string {
+	version := byte(4)
+	uuid := make([]byte, 16)
+	rand.Read(uuid)
+
+	uuid[6] = (uuid[6] & 0x0f) | (version << 4)
+
+	uuid[8] = (uuid[8] & 0xbf) | 0x80
+
+	buf := make([]byte, 36)
+	var dash byte = '-'
+	hex.Encode(buf[0:8], uuid[0:4])
+	buf[8] = dash
+	hex.Encode(buf[9:13], uuid[4:6])
+	buf[13] = dash
+	hex.Encode(buf[14:18], uuid[6:8])
+	buf[18] = dash
+	hex.Encode(buf[19:23], uuid[8:10])
+	buf[23] = dash
+	hex.Encode(buf[24:], uuid[10:])
+
+	return string(buf)
 }
